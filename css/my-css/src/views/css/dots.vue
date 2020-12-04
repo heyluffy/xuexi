845bcb8f74dc75ec68310b77c680835668257e11
beizhuxin <438830119@qq.com>
2020-12-04 16:43:03 +0800

    update

--- css/my-css/src/views/css/dots.vue
+++ css/my-css/src/views/css/dots.vue
@@ -1,8 +1,9 @@
 <template>
   <div ref="dots" class="dots" :class="{ scrollable: scrollable }">
-    <div ref="dotsScroll" class="dots-scroll">
+    <div ref="dotsScroll" class="dots-scroll" :style="scrollStyle">
       <div ref="dotsList" class="dots-list">
         <div
+          ref="dotItem"
           v-for="item in dots"
           :key="item"
           @click="handleClick(item)"
@@ -12,9 +13,9 @@
       </div>
     </div>
     <div class="dots-arrows" v-if="scrollable">
-      <i class="dots-arrows-item fa fa-camera-retro"></i>
+      <i class="dots-arrows-item left fa fa-chevron-left" @click="moveTo('left')"></i>
+      <i class="dots-arrows-item right fa fa-chevron-right" @click="moveTo('right')"></i>
     </div>
-    <i class="dots-arrows-item fa fa-camera-retro"></i>
     <!-- <div class="box">
       <div
       v-for="item in dots"
@@ -33,11 +34,28 @@ export default {
     return {
       dots: 10,
       activeIndex: 0,
-      scrollable: false,
-      resizeObserver: null
+      scrollable: true,
+      resizeObserver: null,
+      isMounted: false,
+      updating: false,
+      dotOffset: 0 // scroll的时候偏移量
     };
   },
+  computed: {
+    containerWidth() {
+      return this.$refs.dotsScroll ? this.$refs.dotsScroll.clientWidth : 0;
+    },
+    contentWidth() {
+      return this.$refs.dotsScroll ? this.$refs.dotsScroll.scrollWidth : 0;
+    },
+    scrollStyle() {
+      return {
+        transform: `translate3d(-${this.dotOffset}px, 0, 0)`
+      };
+    }
+  },
   mounted() {
+    this.isMounted = true;
     this.addResizeObserver();
   },
   updated() {
@@ -49,15 +67,16 @@ export default {
   methods: {
     handleClick(index) {
       this.activeIndex = index;
+      this.moveToCurretn(index);
     },
     update() {
-      const scrollWidth = this.$refs.dotsScroll.clientWidth;
-      const innerWidth = this.$refs.dotsList.offsetWidth;
-      if (scrollWidth < innerWidth) {
+      this.updating = true;
+      if (this.containerWidth < this.contentWidth) {
         this.scrollable = true;
       } else {
         this.scrollable = false;
       }
+      this.updating = false;
     },
     addResizeObserver() {
       if (!this.resizeObserver && this.$refs.dots) {
@@ -68,14 +87,47 @@ export default {
       }
     },
     discResizeObserver() {
-      this.resizeObserver.disconnect();
+      this.resizeObserver && this.resizeObserver.disconnect();
+    },
+    moveTo(type) {
+      if (type === 'left') {
+        this.dotOffset = this.dotOffset > this.containerWidth
+          ? this.dotOffset - this.containerWidth
+          : 0;
+      } else if (type === 'right') {
+        const lastOffset = this.contentWidth - this.containerWidth - this.dotOffset;
+        this.dotOffset += lastOffset > this.containerWidth
+          ? this.containerWidth
+          : lastOffset;
+      }
+    },
+    moveToCurretn(index) {
+      if (!this.mounted) return;
+      const containerBounding = this.$refs.dotsScroll.getBoundingClientRect();
+      const activeBounding = this.$refs.dotItem[index].getBoundingClientRect();
+      const maxOffset = this.containerWidth - this.contentWidth; // 最大偏移量
+      let newOffset = this.dotOffset + (activeBounding.left - containerBounding.left);
+      if (newOffset > maxOffset) {
+        newOffset = maxOffset;
+      } else if (newOffset < 0) {
+        newOffset = 0;
+      }
+      this.dotOffset = newOffset;
     }
   }
 };
 </script>
 <style lang='scss' scoped>
+@import '../../assets/css/var.scss';
+.dots {
+  position: relative;
+  text-align: center;
+}
 .dots.scrollable {
   padding: 0 20px;
+}
+.dots-scroll {
+  overflow: hidden;
 }
 .dots-scroll::after {
   display: table;
@@ -83,7 +135,7 @@ export default {
   clear: both;
 }
 .dots-list {
-  float: left;
+  display: inline-block;
   white-space: nowrap;
 }
 .dots-item {
@@ -94,12 +146,29 @@ export default {
   cursor: pointer;
   vertical-align: middle;
   border-radius: 10px;
-  background: var(--grey-color-1);
+  background: $--grey-color-1;
   transition: 0.3s ease;
   &.active {
     width: 36px;
-    background: var(--purple-color-1);
+    background: $--purple-color-1;
   }
+}
+.dots-arrows-item {
+  position: absolute;
+  top: 50%;
+  color: $--grey-color-2;
+  cursor: pointer;
+  transform: translate3d(0, -50%, 0);
+  transition: 0.3s;
+}
+.dots-arrows-item:hover {
+  color: rgba($--grey-color-2, 0.8);
+}
+.dots-arrows-item.left {
+  left: 5px;
+}
+.dots-arrows-item.right {
+  right: 5px;
 }
 // .dots-item1 {
 //   transition: 0.3s ease;
